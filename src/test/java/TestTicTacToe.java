312fd176e8792334009356c64400cbed8d6c6c5a
Viktor Mandlbauer <viktor.mandlbauer@gmail.com>
2024-06-20 19:29:05 +0200

    Added dummy test

---
+++ src/test/java/TestTicTacToe.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void dummyTest() {
+
+        // do something
+        Assertions.assertEquals(1,1);
+    }
+}
